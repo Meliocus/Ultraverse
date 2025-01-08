3cd22c246452fea944f2c084dbc8f7b22e06570e
Lucas vanessche <ulacs57otaku@gmail.com>
2025-01-08 21:48:23 +0100

    add ultraverse

---
+++ Ultraverse/Pages/register.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Web.Http;
+using Ultraverse.models;
+
+namespace Ultraverse.Pages
+{
+    public class registerModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost() {
+            
+        }
+
+        public bool Register() { 
+            return true; 
+        }
+    }
+}
---
+++ Ultraverse/models/MySQLModel.cs
@@ -0,0 +1,13 @@
+﻿using MySqlConnector;
+
+namespace Ultraverse.models
+{
+    public struct MySQLModel
+    {
+        public required string Server { get; init; }
+        public required string Database { get; init; }
+        public required string Username { get; init; }
+        public required string Password { get; init; }
+        public string ConnectionString => $"server={Server};database={Database};uid={Username};pwd={Password};";
+    }
+}
